c9365928d67c4ec044a15369694d4b0e9959e94d
Ivan <mvzxw2@gmail.com>
2026-01-07 16:16:07 +0300

    add in userModel role

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    role: { type: String, required: true, default: 'user' }
 }, {
     timestamps: true
 })
